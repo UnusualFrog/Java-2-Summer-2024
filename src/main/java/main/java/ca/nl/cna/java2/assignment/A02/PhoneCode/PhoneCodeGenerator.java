714171bb33dfd229293fc16dbce45d24b9bc0134
Noah Forward <forwardnoah@gmail.com>
2024-06-01 21:03:13 -0230

    Partial working recursion for phone combos a02

--- src/main/java/main/java/ca/nl/cna/java2/assignment/A02/PhoneCode/PhoneCodeGenerator.java
+++ src/main/java/main/java/ca/nl/cna/java2/assignment/A02/PhoneCode/PhoneCodeGenerator.java
@@ -6,32 +6,43 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class PhoneCodeGenerator {
-    private final HashMap<String, String[]> phoneCodes = new HashMap<>();
+    private final HashMap<Integer, String[]> phoneCodes = new HashMap<>();
 
     public PhoneCodeGenerator() {
-        phoneCodes.put("2",new String[]{"A","B","C"});
-        phoneCodes.put("3",new String[]{"D","E","F"});
-        phoneCodes.put("4",new String[]{"G","H","I"});
-        phoneCodes.put("5",new String[]{"J","K","L"});
-        phoneCodes.put("6",new String[]{"M","N","O"});
-        phoneCodes.put("7",new String[]{"P","R","S"});
-        phoneCodes.put("8",new String[]{"T","U","V"});
-        phoneCodes.put("9",new String[]{"W","X","Y"});
+        phoneCodes.put(2,new String[]{"A","B","C"});
+        phoneCodes.put(3,new String[]{"D","E","F"});
+        phoneCodes.put(4,new String[]{"G","H","I"});
+        phoneCodes.put(5,new String[]{"J","K","L"});
+        phoneCodes.put(6,new String[]{"M","N","O"});
+        phoneCodes.put(7,new String[]{"P","R","S"});
+        phoneCodes.put(8,new String[]{"T","U","V"});
+        phoneCodes.put(9,new String[]{"W","X","Y"});
     }
 
-    public HashMap<String, String[]> getPhoneCodes() {
+    public HashMap<Integer, String[]> getPhoneCodes() {
         return phoneCodes;
     }
 
     public void generateComboLogs(String userInput) {
         try (Formatter output = new Formatter("./logs/phone/phone_combos_"+ userInput +".txt")) {
-            output.format("Original Phone Number: %s", userInput);
-            for (int i = 0; i < userInput.length(); i++) {
-                System.out.println(phoneCodes.get(Character.toString(userInput.charAt(i)))[0]);
-            }
+            output.format("Original Phone Number: %s \n", userInput);
+            generateCombo(output, userInput, "", 0, 0);
 
         } catch (FileNotFoundException e) {
             System.err.println(e);
+        }
+    }
+
+    private void generateCombo(Formatter output, String inputCode, String outputCode, int inputIndex, int hashIndex) {
+        int currentDigit = Integer.parseInt(inputCode.charAt(inputIndex) + "");
+        if (inputIndex == inputCode.length()-1) {
+            output.format("%s\n", outputCode);
+            return;
+        } else {
+            for (int i = 0; i < 3; i++) {
+                generateCombo(output, inputCode, outputCode + this.getPhoneCodes().get(currentDigit)[i], inputIndex + 1, i);
+            }
+
         }
     }
 
@@ -44,13 +55,13 @@ public class PhoneCodeGenerator {
         String userInput = scanner.nextLine();
         String[] numberParts = userInput.split("-");
 
+        // Handle dash in phone number
         String userNumber = "";
         if (numberParts.length > 1) {
             userNumber = numberParts[0] + numberParts[1];
         } else {
             userNumber = numberParts[0];
         }
-
 
         generator.generateComboLogs(userNumber);
     }
