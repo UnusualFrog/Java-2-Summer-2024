b1a84e470b7994d3e7ac034c23b8e309472c4af7
Forward <forwardnoah@gmail.com>
2024-06-05 10:26:43 -0230

    Regex exercises

---
+++ src/main/java/main/java/ca/nl/cna/java2/exercise/ex6_StringRegex/RegexPractice.java
@@ -0,0 +1,35 @@
+package main.java.ca.nl.cna.java2.exercise.ex6_StringRegex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexPractice {
+    public static void main(String[] args) {
+        String regex = "\\w*abc";
+        Pattern expression = Pattern.compile(regex);
+
+        String name = "abcdefghijkabcd";
+
+        Matcher matcher = expression.matcher(name);
+
+        System.out.println(matcher.matches());
+
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+
+
+//        String name = "Eric Stock";
+//        String name2 = "ErickStock";
+//
+//        //String regex = "[A-Za-z\\s]+";
+//        //String regex = "[A-Za-z]+\\s[A-Za-z]+";
+//        //String regex = "Hi (Eric|Stock)";
+//        //String regex = "[A-Za-z]{4}\\s[A-Za-z]{2}";
+//        String regex = "[A-Za-z]{4}\\s?[A-Za-z]+";
+//
+//        System.out.println(name.matches(regex));
+//        System.out.println(name2.matches(regex));
+
+    }
+}
---
+++ src/main/java/main/java/ca/nl/cna/java2/exercise/ex6_StringRegex/StringBuilderTest.java
@@ -0,0 +1,11 @@
+package main.java.ca.nl.cna.java2.exercise.ex6_StringRegex;
+
+public class StringBuilderTest {
+
+
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder("Eric Stock");
+        stringBuilder.deleteCharAt(4);
+        System.out.println(stringBuilder);
+    }
+}
