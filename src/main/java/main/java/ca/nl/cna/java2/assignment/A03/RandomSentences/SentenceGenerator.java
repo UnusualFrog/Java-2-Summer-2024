d166dbf5834d36ee0f8ce68aa8cebe28bf3a00d2
Noah Forward <forwardnoah@gmail.com>
2024-06-05 19:44:33 -0230

    A03 Q1 & Q2 Random Sentence & Reverse sentence complete

---
+++ src/main/java/main/java/ca/nl/cna/java2/assignment/A03/RandomSentences/SentenceGenerator.java
@@ -0,0 +1,40 @@
+package main.java.ca.nl.cna.java2.assignment.A03.RandomSentences;
+
+import java.util.Random;
+
+public class SentenceGenerator {
+    private final String[] articles = {"the", "a", "one", "some", "any"};
+    private final String[] nouns = {"boy", "girl", "dog", "town", "car"};
+    private final String[] verbs = {"drove", "jumped", "ran", "walked", "skipped"};
+    private final String[] prepositions = {"to", "from", "over", "under", "on"};
+
+    public String generateSentence() {
+        Random random = new Random();
+        String outputSentence = "";
+
+        String firstWord = articles[random.nextInt(0,articles.length)];
+        firstWord = firstWord.substring(0,1).toUpperCase() + firstWord.substring(1);
+        outputSentence += firstWord;
+        outputSentence += " ";
+        outputSentence += nouns[random.nextInt(0,nouns.length)];
+        outputSentence += " ";
+        outputSentence += verbs[random.nextInt(0,verbs.length)];
+        outputSentence += " ";
+        outputSentence += prepositions[random.nextInt(0,prepositions.length)];
+        outputSentence += " ";
+        outputSentence += articles[random.nextInt(0,articles.length)];
+        outputSentence += " ";
+        outputSentence += nouns[random.nextInt(0,nouns.length)];
+        outputSentence += ".";
+
+        return outputSentence;
+    }
+
+    public static void main(String[] args) {
+        SentenceGenerator generator = new SentenceGenerator();
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println(generator.generateSentence());
+        }
+    }
+}
---
+++ src/main/java/main/java/ca/nl/cna/java2/assignment/A03/ReverseSentence/ReverseSentence.java
@@ -0,0 +1,26 @@
+package main.java.ca.nl.cna.java2.assignment.A03.ReverseSentence;
+
+import java.util.Scanner;
+
+public class ReverseSentence {
+
+    public static String reverseSentence(String sentence) {
+        String[] words = sentence.split(" ");
+        StringBuilder outputSentence = new StringBuilder();
+
+        for (int i = words.length-1; i >= 0; i--) {
+            outputSentence.append(words[i]);
+            outputSentence.append(" ");
+        }
+
+        return outputSentence.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a sentence to reverse: ");
+        String userSentence = scanner.nextLine();
+        System.out.println(ReverseSentence.reverseSentence(userSentence));
+    }
+}
