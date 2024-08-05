36651a580c1b5c0e8342af27b799c3e97b0b641e
Forward <forwardnoah@gmail.com>
2024-08-05 12:49:55 -0230

    Seperated player from client

--- src/main/java/main/java/ca/nl/cna/java2/project/Player.java
+++ src/main/java/main/java/ca/nl/cna/java2/project/Player.java
@@ -13,7 +13,7 @@ import org.json.simple.parser.ParseException;
 
 /**
  * This class represents a player in a Blackjack game.
- * It manages the player's current hand, bet money, and current bet, and includes a main method to connect to the Blackjack server.
+ * It manages the player's current hand, bet money, and current bet
  *
  * @author Noah.Forward
  */
@@ -89,59 +89,4 @@ public class Player {
         this.currentBet = currentBet;
     }
 
-    /**
-     * Main method to connect to the Blackjack server.
-     *
-     * @param args command-line arguments
-     * @throws IOException if an I/O error occurs when creating the socket
-     */
-    public static void main(String[] args) throws IOException {
-
-        String hostName = "localhost";
-        int portNumber = 4401;
-
-        // Establish socket connection, output for sending messages to server, and input for receiving messages from the server
-        try (Socket kkSocket = new Socket(hostName, portNumber);
-             PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(kkSocket.getInputStream()))) {
-
-            // Stream for reading user input
-            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-
-            // Store server and user input respectively
-            String fromServer;
-            String fromServerMsg;
-            String fromUser;
-
-            JSONParser parser = new JSONParser();
-            JSONObject jsonResponse;
-
-            while ((fromServer = in.readLine()) != null) {
-                // Parse JSON object for server response msg
-                try {
-                    jsonResponse = (JSONObject) parser.parse(fromServer);
-                } catch (ParseException e) {
-                    throw new RuntimeException(e);
-                }
-                fromServerMsg = (String) jsonResponse.get("string");
-
-                // Output response from server
-                if (!fromServerMsg.equals("continue")) {
-                    System.out.println(fromServerMsg);
-                } else {
-                    // Allow for user input if server msg is "continue"
-                    fromUser = stdIn.readLine();
-                    if (fromUser != null) {
-                        out.println(fromUser);
-                    }
-                }
-            }
-        } catch (UnknownHostException e) {
-            System.err.println("Don't know about host " + hostName);
-            System.exit(1);
-        } catch (IOException e) {
-            System.err.println("Couldn't get I/O for the connection to " + hostName);
-            System.exit(1);
-        }
-    }
 }
